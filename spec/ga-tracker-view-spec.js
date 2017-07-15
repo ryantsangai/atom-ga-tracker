0b2a80181599812cefd9061f5a3b651a04c74ef0
Ryan Tsang <ryantsangai@gmail.com>
2017-07-15 10:29:45 +0800

    init from atom

---
+++ spec/ga-tracker-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GaTrackerView from '../lib/ga-tracker-view';
+
+describe('GaTrackerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
